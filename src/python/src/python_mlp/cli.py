c16d54160eb27c382494d6f768ed3b5602e0da2e
Miguel Villa Floran <miguel.villafloran@gmail.com>
2025-03-19 20:01:03 -0700

    Fix

--- src/python/src/python_mlp/cli.py
+++ src/python/src/python_mlp/cli.py
@@ -1,13 +1,11 @@
 import typer
 
-from core import return_two
-
 app = typer.Typer()
 
 
 @app.command()
 def run():
-    print(f"Here is {return_two()} for you")
+    print("Hello, world!")
 
 
 def entrypoint():
